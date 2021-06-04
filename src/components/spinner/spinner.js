1c019b331ecb548ea3bc6d6f3b02461126d549ce
igor <usd.wmz@gmail.com>
2021-06-04 11:50:52 +0300

    add random char, spinner, error message

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css';
+const Spinner = () => {
+	return (
+		<div class="loadingio-spinner-spin-6606rsz3lf8">
+			<div class="ldio-g4nz50xkkwn">
+				<div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+			</div>
+		</div>
+
+	)
+}
+
+
+export default Spinner;
